7ab64f383fbf7d9cfb2792bddc66f927a59da566
BlazePhenom <saxmax230505@gmail.com>
2024-10-31 01:59:43 +0530

    Update BubbleSort.java

--- JAVA/BubbleSort.java
+++ JAVA/BubbleSort.java
@@ -1,11 +1,11 @@
-public class BubbleSort {
+public class BubbleSort { //The name given to this algorithm is due to this concept that after performing this algorithm each time,the largest element of the array comes at the last position,similar to a bubble when it comes to the surfacetop of a water body
     public static void bubbleSort(int[] arr) {
-        int n = arr.length;
+        int N = arr.length;
         
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
+        for (int i = 0; i < N - 1; i++) {  //This outer loop is for performing the sorting operation till the array(arr) is completely sorted.
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) { //We are checking here for two consecutive numbers and swapping them if the condition is true
+                    int temp = arr[j]; //the temp variable has been declared and initialised to do the swapping procedure.
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
